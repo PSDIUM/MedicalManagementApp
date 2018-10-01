4fa41a48999d0b26138535816e9ccfb153b6f6d9
squabbi <squabbi@users.noreply.github.com>
2018-10-01 21:41:32 +1000

    Added getters and setters

--- app/src/main/java/com/sepproject/medicalmanagementapp/model/Drug.java
+++ app/src/main/java/com/sepproject/medicalmanagementapp/model/Drug.java
@@ -17,4 +17,36 @@ public class Drug {
         this.mPrice = price;
         this.mSideEffects = sideEffects;
     }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getRecommendedDosage() {
+        return mRecommendedDosage;
+    }
+
+    public void setRecommendedDosage(String recommendedDosage) {
+        mRecommendedDosage = recommendedDosage;
+    }
+
+    public Double getPrice() {
+        return mPrice;
+    }
+
+    public void setPrice(Double price) {
+        mPrice = price;
+    }
+
+    public String getSideEffects() {
+        return mSideEffects;
+    }
+
+    public void setSideEffects(String sideEffects) {
+        mSideEffects = sideEffects;
+    }
 }

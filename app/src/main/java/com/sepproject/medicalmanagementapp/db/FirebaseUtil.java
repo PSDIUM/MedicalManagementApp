099de7d42e57fbdf36b906a2902d06d8a9f056ba
squabbi <squabbi@users.noreply.github.com>
2018-10-01 21:41:00 +1000

    Implemented singleton and getUser auth

--- app/src/main/java/com/sepproject/medicalmanagementapp/db/FirebaseUtil.java
+++ app/src/main/java/com/sepproject/medicalmanagementapp/db/FirebaseUtil.java
@@ -1,4 +1,34 @@
 package com.sepproject.medicalmanagementapp.db;
 
+import android.support.annotation.Nullable;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class FirebaseUtil {
+
+    private static FirebaseUtil mFirebaseUtil;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+
+    // Singleton instance getter
+    public static FirebaseUtil getInstance() {
+
+        if (mFirebaseUtil == null) {
+            mFirebaseUtil = new FirebaseUtil();
+        }
+        return mFirebaseUtil;
+    }
+
+    // Private constructor to initialise auth and database
+    private FirebaseUtil() {
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+    }
+
+    @Nullable
+    public FirebaseUser getCurrentUser() {
+        return mAuth.getCurrentUser();
+    }
 }

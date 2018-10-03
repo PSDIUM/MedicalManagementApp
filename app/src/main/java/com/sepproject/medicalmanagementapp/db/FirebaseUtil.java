fe32e951fee1a5be6f380236a7254f86c66c3d8c
squabbi <squabbi@users.noreply.github.com>
2018-10-03 10:24:47 +1000

    Revert "Added login methods"
    This reverts commit 3560b9fb2444d10e3a3898e91a7c5d45a3be862a.

--- app/src/main/java/com/sepproject/medicalmanagementapp/db/FirebaseUtil.java
+++ app/src/main/java/com/sepproject/medicalmanagementapp/db/FirebaseUtil.java
@@ -1,12 +1,7 @@
 package com.sepproject.medicalmanagementapp.db;
 
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -14,13 +9,9 @@ import com.google.firebase.firestore.Query;
 
 public class FirebaseUtil {
 
-    private static final String TAG = "FirebaseUtil";
-
     private static FirebaseUtil mFirebaseUtil;
     private FirebaseAuth mAuth;
     private FirebaseFirestore mFirestore;
-
-    private boolean mLoginResult = false;
 
     private static final String DRUG_COLLECTION = "drugs";
 
@@ -39,35 +30,10 @@ public class FirebaseUtil {
         mFirestore = FirebaseFirestore.getInstance();
     }
 
-    // Login/user methods
     @Nullable
     public FirebaseUser getCurrentUser() {
         return mAuth.getCurrentUser();
     }
-
-    public boolean logIn(String email, String password) {
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Log.d(TAG, "signInWithEmail:success");
-                            setLoginResult(true);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            setLoginResult(false);
-                        }
-                    }
-                });
-
-        return mLoginResult;
-    }
-
-    private void setLoginResult(boolean success) {
-        mLoginResult = success;
-    }
-
 
     public Boolean isDoctor(FirebaseUser user) {
         return true;

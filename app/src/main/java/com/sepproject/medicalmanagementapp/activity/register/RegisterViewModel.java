aad52780f0c0ac1eb22218e5cf26d9adc503e636
squabbi <squabbi@users.noreply.github.com>
2018-10-03 11:12:15 +1000

    Implemented base register user

--- app/src/main/java/com/sepproject/medicalmanagementapp/activity/register/RegisterViewModel.java
+++ app/src/main/java/com/sepproject/medicalmanagementapp/activity/register/RegisterViewModel.java
@@ -2,5 +2,13 @@ package com.sepproject.medicalmanagementapp.activity.register;
 
 import android.arch.lifecycle.ViewModel;
 
+import com.sepproject.medicalmanagementapp.db.FirebaseUtil;
+
 public class RegisterViewModel extends ViewModel {
+
+    FirebaseUtil mFirebaseUtil = FirebaseUtil.getInstance();
+
+    public boolean registerUser(String email, String password) {
+        return mFirebaseUtil.registerUser(email, password);
+    }
 }

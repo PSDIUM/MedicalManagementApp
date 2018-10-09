c9f5dd130d0c3f35a1a8126f4ecec3b258afe663
squabbi <squabbi@users.noreply.github.com>
2018-10-09 13:39:14 +1100

    Created PatientAdapter for RecyclerView

---
+++ app/src/main/java/com/sepproject/medicalmanagementapp/navigation/PatientAdapter.java
@@ -0,0 +1,6 @@
+package com.sepproject.medicalmanagementapp.navigation;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+
+public class PatientAdapter extends FirestoreRecyclerAdapter {
+}

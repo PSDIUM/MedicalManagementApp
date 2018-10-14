38f846e44a7e389c9b9a8382c08658fd6d5edcfa
rromano <rromanomusic@gmail.com>
2018-10-15 07:44:21 +1100

    Slight changes to code layout

--- app/src/main/java/com/sepproject/medicalmanagementapp/login/LoginActivity.java
+++ app/src/main/java/com/sepproject/medicalmanagementapp/login/LoginActivity.java
@@ -31,7 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         // Register ViewModel

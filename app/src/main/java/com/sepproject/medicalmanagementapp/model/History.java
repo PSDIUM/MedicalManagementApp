dacf8bccc3ad74d66bdb2da662383307c53aec53
rromano <rromanomusic@gmail.com>
2018-10-19 18:19:48 +1100

    Fixed history model

--- app/src/main/java/com/sepproject/medicalmanagementapp/model/History.java
+++ app/src/main/java/com/sepproject/medicalmanagementapp/model/History.java
@@ -1,26 +1,41 @@
 package com.sepproject.medicalmanagementapp.model;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 public class History {
 
-    private Date mDate;
+    private String mDate;
+    private String mTime;
     private String mDescription;
 
+    History(){
+
+    }
+
     public History(String description) {
-        mDate = new Date();
+        Date date = new Date();
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+        mTime = timeFormat.format(date);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+        mDate = dateFormat.format(date);
         mDescription = description;
     }
 
     public String getTime(){
-        return String.valueOf(mDate.getTime());
+        return mTime;
     }
 
     public String getDate(){
-        SimpleDateFormat date = new SimpleDateFormat("dd-MMM-yyyy");
-        return date.format(mDate.getTime());
+        return mDate;
+    }
+
+    public void setDate(String date) {
+        mDate = date;
+    }
+
+    public void setTime(String time) {
+        mTime = time;
     }
 
     public String getDescription() {

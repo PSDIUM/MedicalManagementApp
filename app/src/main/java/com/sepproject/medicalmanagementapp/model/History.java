7bc757dfa4d26bccf73a5c9f01f5ec1725775e14
rromano <rromanomusic@gmail.com>
2018-10-19 12:32:49 +1100

    added data to the model

--- app/src/main/java/com/sepproject/medicalmanagementapp/model/History.java
+++ app/src/main/java/com/sepproject/medicalmanagementapp/model/History.java
@@ -1,8 +1,33 @@
 package com.sepproject.medicalmanagementapp.model;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class History {
 
-    public History() {
+    private Date mDate;
+    private String mDescription;
 
+    public History(String description) {
+        mDate = new Date();
+        mDescription = description;
+    }
+
+    public String getTime(){
+        return String.valueOf(mDate.getTime());
+    }
+
+    public String getDate(){
+        SimpleDateFormat date = new SimpleDateFormat("dd-MMM-yyyy");
+        return date.format(mDate.getTime());
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
     }
 }

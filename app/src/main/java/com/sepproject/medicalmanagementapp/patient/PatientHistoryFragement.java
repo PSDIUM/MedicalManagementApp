266ed3bf59404aea9dd77e1c3d9af065788d3231
rromano <rromanomusic@gmail.com>
2018-10-19 18:20:58 +1100

    Added start add appointment

--- app/src/main/java/com/sepproject/medicalmanagementapp/patient/PatientHistoryFragement.java
+++ app/src/main/java/com/sepproject/medicalmanagementapp/patient/PatientHistoryFragement.java
@@ -2,20 +2,26 @@ package com.sepproject.medicalmanagementapp.patient;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.sepproject.medicalmanagementapp.R;
+import com.sepproject.medicalmanagementapp.appointment.AddAppointmentActivity;
+import com.sepproject.medicalmanagementapp.appointment.AddAppointmentFragment;
+import com.sepproject.medicalmanagementapp.model.History;
 import com.sepproject.medicalmanagementapp.model.User;
-import com.sepproject.medicalmanagementapp.navigation.LookupListAdaptor;
+
+import java.util.List;
 
 public class PatientHistoryFragement extends Fragment {
 
@@ -25,6 +31,7 @@ public class PatientHistoryFragement extends Fragment {
     private TextView mLastName;
     private TextView mId;
     private TextView mAppointmentTitle;
+    private User mCurrentUser;
     private RecyclerView mRecyclerView;
     private AppointmentListAdaptor mAdapter;
 
@@ -35,17 +42,28 @@ public class PatientHistoryFragement extends Fragment {
 
         mPatientNavigationViewModel = ViewModelProviders.of(getActivity()).get(PatientNavigationViewModel.class);
         mPatientNavigationViewModel.setPatient(args.getString("USER_EMAIL"));
+        mPatientNavigationViewModel.setPatientAppointments(args.getString("USER_EMAIL"));
 
         mTitle = view.findViewById(R.id.patient_history_title);
         mFirstName = view.findViewById(R.id.patient_history_first_name);
         mLastName = view.findViewById(R.id.patient_history_last_name);
         mId = view.findViewById(R.id.patient_history_id);
         mAppointmentTitle = view.findViewById(R.id.patient_history_appointment_title);
+        FloatingActionButton fab = view.findViewById(R.id.patient_history_fb);
 
         RecyclerView recyclerView = view.findViewById(R.id.patient_history_rv);
         mAdapter = new AppointmentListAdaptor(getActivity());
         recyclerView.setAdapter(mAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        mPatientNavigationViewModel.getAllAppointments().observe(this, new Observer<List<History>>() {
+            @Override
+            public void onChanged(@Nullable List<History> histories) {
+                if(histories.size()!=0) {
+                    mAdapter.setHistory(histories);
+                }
+            }
+        });
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -54,17 +72,29 @@ public class PatientHistoryFragement extends Fragment {
             }
         },500);
 
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAddAppointment();
+            }
+        });
+
         return view;
     }
 
     private void setPatient(){
-        mPatientNavigationViewModel.getPatient().observe(this, new Observer<User>() {
+        mPatientNavigationViewModel.getPatient().observe(getActivity(), new Observer<User>() {
             @Override
             public void onChanged(User patient) {
+                mCurrentUser = patient;
                 setPatientDetails(patient);
             }
         });
     }
+
+    private void setAppointments(){
+    }
+
     private void setPatientDetails(User patient){
         mTitle.setText(patient.getName());
         String[] patientName = patient.getName().split(" ");
@@ -72,5 +102,11 @@ public class PatientHistoryFragement extends Fragment {
         mLastName.setText(patientName[1]);
         mId.setText(String.valueOf(patient.getId()));
         mAppointmentTitle.setText(patient.getName() + "'s \n Previous Appointments");
+    }
+
+    private void startAddAppointment(){
+        Intent i = new Intent(getActivity(), AddAppointmentActivity.class);
+        i.putExtra("USER_EMAIL", mCurrentUser.getEmail());
+        startActivity(i);
     }
 }
